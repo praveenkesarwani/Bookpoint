a782a26cfc98816fcd6cb9cce01b1431741c5f1b
Praveen Kesarwani <praveenkesarwani739@gmail.com>
2020-07-23 10:37:35 +0530

    final commit

--- resources/js/script.js
+++ resources/js/script.js
@@ -42,25 +42,25 @@ $(document).ready(function() {
     
     /* Animations on scroll */
     $('.js--wp-1').waypoint(function(direction) {
-        $('.js--wp-1').addClass('animated fadeIn');
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn');
     }, {
         offset: '50%'
     });
     
     $('.js--wp-2').waypoint(function(direction) {
-        $('.js--wp-2').addClass('animated fadeInUp');
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
     }, {
         offset: '50%'
     });
     
     $('.js--wp-3').waypoint(function(direction) {
-        $('.js--wp-3').addClass('animated fadeIn');
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
     }, {
         offset: '50%'
     });
     
     $('.js--wp-4').waypoint(function(direction) {
-        $('.js--wp-4').addClass('animated pulse');
+        $('.js--wp-4').addClass('animate__animated animate__pulse');
     }, {
         offset: '50%'
     });
